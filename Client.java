4012cac99266aae7dc15cd04f6349af92b0bc1aa
snalbreiki <86579970+snalbreiki@users.noreply.github.com>
2024-10-23 00:36:15 +0400

    Update 3 Client.java

--- Client.java
+++ Client.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.sql.Timestamp;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
@@ -9,24 +11,39 @@ public class Client {
             clientSocket = new DatagramSocket();  // Create client socket
             InetAddress serverAddress = InetAddress.getByName("localhost");  // Server address
 
-            // Construct email message
-            String email = "From: Client\n" +
-                    "To: Server\n" +
-                    "Subject: Test\n" +
-                    "Body: Helllooo\n";
+            System.out.println ("- Mail Client starting on host: " + InetAddress.getLocalHost().getHostName());
+            System.out.println ("- Type name of Mail server: " + serverAddress);
 
-            byte[] sendData = email.getBytes();  // Convert email to bytes
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 1234);
-            clientSocket.send(sendPacket);  // Send email packet
+            while (true) {
+                // write email message
+                Scanner cin = new Scanner ( System.in );
+                System.out.println ( "---------------------\n" +
+                                     "Creating New Email.." );
 
-            byte[] receiveData = new byte[1024];  // Buffer for server response
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);  // Receive response
+                System.out.print ( "To: " );
+                String to = cin.nextLine ( );
+                System.out.print ( "From: " );
+                String from = cin.nextLine ( );
+                System.out.print ( "Subject: " );
+                String subject = cin.nextLine ( );
+                System.out.print ( "Body: " );
+                String body = cin.nextLine ( );
 
-            // Print server's response
-            String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            System.out.println("Server response: " + response);
+                String timestamp = java.time.LocalDateTime.now ( ).toString ( );
+                String email = String.format ( "From: %s\nTo: %s\nSubject: %s\nBody: %s\nTimestamp: %s\n" , to , from , subject , body , timestamp );
 
+                byte[] sendData = email.getBytes ( );  // Convert email to bytes
+                DatagramPacket sendPacket = new DatagramPacket ( sendData , sendData.length , serverAddress , 12345 );
+                clientSocket.send ( sendPacket );  // Send email packet
+
+                byte[] receiveData = new byte[ 1024 ];  // Buffer for server response
+                DatagramPacket receivePacket = new DatagramPacket ( receiveData , receiveData.length );
+                clientSocket.receive ( receivePacket );  // Receive response
+
+                // Print server's response
+                String response = new String ( receivePacket.getData ( ) , 0 , receivePacket.getLength ( ) );
+                System.out.println ( "- Server response: " + response );
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
